b83784bc5269559398bc0acfce61a6edc7d7019e
Ann Paul <annpaul126@gmail.com>
2024-11-22 15:00:36 -0500

    removed all files containing IP address from 2 folders

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -44,6 +44,8 @@ cp ./data/raw/event_log*.log  ./data/processed/event_logs
 cp ./data/raw/user_log*.log  ./data/processed/user_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
+rm ./data/raw/*ipaddr*.*
+rm ./data/processed/user_logs/*ipaddr*.*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
